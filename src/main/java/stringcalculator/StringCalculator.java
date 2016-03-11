453d4c7a76a34e4a1244e014d64f3ae72d503160
Marcus Michaud <mmichaud.pro@gmail.com>
2016-03-11 12:18:55 +0100

    exo step 1

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -2,8 +2,25 @@ package stringcalculator;
 
 public class StringCalculator {
 
+	private static int NUMBER1 = 0;
+	private static int NUMBER2 = 0;
+	
 	public int add(String numbers) {
-		return -1;
+		
+		if (numbers != "") {
+			String[] splitNumbers = numbers.split(",");
+			
+			switch (splitNumbers.length) {
+				case 1: 
+					NUMBER1 = Integer.parseInt(splitNumbers[0]);
+					break;
+				case 2:
+					NUMBER1 = Integer.parseInt(splitNumbers[0]);
+					NUMBER2 = Integer.parseInt(splitNumbers[1]);
+					break;
+			}
+		}
+		return NUMBER1 + NUMBER2;
 	}
 
 }
--- src/test/java/stringcalculator/StringCalculatorTest.java
+++ src/test/java/stringcalculator/StringCalculatorTest.java
@@ -8,13 +8,31 @@ public class StringCalculatorTest {
 
 	private StringCalculator sc = new StringCalculator();
 
-	@Test
-	public void check_add() {
+	@Test public void 
+	check_if_add_is_empty() {
+		// Test
+		int result = sc.add("");
+
+		// Assertions
+		assertThat(result).isEqualTo(0);
+	}
+	
+	@Test public void 
+	check_if_add_contains_one_number() {
 		// Test
 		int result = sc.add("1");
 
 		// Assertions
-		assertThat(result).isEqualTo(-1);
+		assertThat(result).isEqualTo(1);
+	}
+	
+	@Test public void 
+	check_if_add_contains_two_numbers() {
+		// Test
+		int result = sc.add("1,2");
+
+		// Assertions
+		assertThat(result).isEqualTo(3);
 	}
 
 }

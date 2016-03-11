0866c6380e63faaf638387728be51bf09c58e539
Marcus Michaud <mmichaud.pro@gmail.com>
2016-03-11 13:11:33 +0100

    exo step 4 - pas complet

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -1,13 +1,19 @@
 package stringcalculator;
 
+import java.util.regex.*;
+
 public class StringCalculator {
 	
+	private static Pattern p;    
+    private static String EMPTY = "";
 	private static String REGEX = ",|\\n";
 	
-	public int add(String... numbers) {
+	public int add(CharSequence numbers) {
 		int result = 0;
-		if (numbers[0] != "") {
-			String[] splitNumbers = numbers[0].split(REGEX);
+		if (numbers != EMPTY) {
+			
+			p = Pattern.compile(REGEX);
+			String[] splitNumbers = p.split(numbers);
 			
 			for(String number : splitNumbers) {
 				result+= Integer.parseInt(number);
@@ -15,5 +21,4 @@ public class StringCalculator {
 		}
 		return result;
 	}
-
 }
